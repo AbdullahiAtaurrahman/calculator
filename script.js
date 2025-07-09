109667cb96a42a67883067e08858b7cdacd3dfe0
unknown <atauraahmad@gmail.com>
2025-07-09 22:19:20 +0100

    created a skeleton of the calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+let num1 = input();
+let operator = input();
+let num2 = input();
+
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function add(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, num1, num2) {
+    add(num1, num2)
+}
