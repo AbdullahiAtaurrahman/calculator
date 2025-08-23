09e26cd83a9532c102a8c3dc6e23dadffd1aaf8b
ataur <atauraahmad@gmail.com>
2025-08-24 00:01:52 +0100

    create funcs for arithmetics

---
+++ script.js
@@ -0,0 +1,36 @@
+const add = (a, b) => {
+    return a + b;
+};
+const subtract = (a, b) => {
+    return a - b;
+};
+const multiply = (a, b) => {
+    return a * b;
+};
+const divide = (a, b) => {
+    return a / b;
+};
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) {
+                return "Error: Division by zero";
+            } else {
+                return divide(a, b);
+            }
+        default:
+            return null;
+    }
+    
+}
+
+
